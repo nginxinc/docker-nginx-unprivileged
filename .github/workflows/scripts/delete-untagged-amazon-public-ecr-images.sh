62854e40df110f4b0eb6382622a627470b388180
Alessandro Fael Garcia <alessfg@hotmail.com>
2023-12-20 21:09:20 +0100

    Update delete-untagged-amazon-public-ecr-images.sh

--- .github/workflows/scripts/delete-untagged-amazon-public-ecr-images.sh
+++ .github/workflows/scripts/delete-untagged-amazon-public-ecr-images.sh
@@ -5,7 +5,7 @@
 set -exu
 
 REPOSITORY_NAME=nginx-unprivileged
-BATCH_DELETE_SIZE=100
+BATCH_DELETE_SIZE=100 # The max delete size allowed by the 'batch-delete-image' aws CLI command is 100
 
 function batch_delete {
   while read -r batch; do
@@ -19,6 +19,8 @@ function batch_delete {
   done < <(xargs -L ${BATCH_DELETE_SIZE} <<<"$1")
 }
 
+# Find untagged manifest lists and delete them first as
+# otherwise any referenced untagged images can not be deleted.
 IMAGE_DIGESTS=$(aws ecr-public describe-images \
   --repository-name "${REPOSITORY_NAME}" \
   --query 'imageDetails[?!imageTags && (contains(imageManifestMediaType, `manifest.list.v2`) || contains(imageManifestMediaType, `image.index.v1`))].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
@@ -26,6 +28,7 @@ IMAGE_DIGESTS=$(aws ecr-public describe-images \
 
 batch_delete "${IMAGE_DIGESTS}"
 
+# Find untagged images and delete them.
 IMAGE_DIGESTS=$(aws ecr-public describe-images \
   --repository-name "${REPOSITORY_NAME}" \
   --query 'imageDetails[?!imageTags].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
