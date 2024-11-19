d1f9427a4fba8fb3a20ba7517dcd53c325d51377
Alexandre Galtier <agaltier@netskope.com>
2024-11-19 20:03:33 +0100

    Entrypoint Fixes
    * fix exec check on .envsh files
    * work with /usr/bin/env rather than sh/bash directly
    * chang `$VAR` to `${VAR}` for better read

--- stable/debian/20-envsubst-on-templates.sh
+++ stable/debian/20-envsubst-on-templates.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 set -e
 
-ME=$(basename "$0")
+ME=$(basename "${0}")
 
 entrypoint_log() {
   if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
@@ -13,16 +13,16 @@ entrypoint_log() {
 add_stream_block() {
   local conffile="/etc/nginx/nginx.conf"
 
-  if grep -q -E "\s*stream\s*\{" "$conffile"; then
-    entrypoint_log "$ME: $conffile contains a stream block; include $stream_output_dir/*.conf to enable stream templates"
+  if grep -q -E "\s*stream\s*\{" "${conffile}"; then
+    entrypoint_log "${ME}: ${conffile} contains a stream block; include ${stream_output_dir}/*.conf to enable stream templates"
   else
     # check if the file can be modified, e.g. not on a r/o filesystem
-    touch "$conffile" 2>/dev/null || { entrypoint_log "$ME: info: can not modify $conffile (read-only file system?)"; exit 0; }
-    entrypoint_log "$ME: Appending stream block to $conffile to include $stream_output_dir/*.conf"
-    cat << END >> "$conffile"
-# added by "$ME" on "$(date)"
+    touch "${conffile}" 2>/dev/null || { entrypoint_log "${ME}: info: can not modify ${conffile} (read-only file system?)"; exit 0; }
+    entrypoint_log "${ME}: Appending stream block to ${conffile} to include ${stream_output_dir}/*.conf"
+    cat << END >> "${conffile}"
+# added by "${ME}" on "$(date)"
 stream {
-  include $stream_output_dir/*.conf;
+  include ${stream_output_dir}/*.conf;
 }
 END
   fi
@@ -38,37 +38,37 @@ auto_envsubst() {
 
   local template defined_envs relative_path output_path subdir
   defined_envs=$(printf '${%s} ' $(awk "END { for (name in ENVIRON) { print ( name ~ /${filter}/ ) ? name : \"\" } }" < /dev/null ))
-  [ -d "$template_dir" ] || return 0
-  if [ ! -w "$output_dir" ]; then
-    entrypoint_log "$ME: ERROR: $template_dir exists, but $output_dir is not writable"
+  [ -d "${template_dir}" ] || return 0
+  if [ ! -w "${output_dir}" ]; then
+    entrypoint_log "${ME}: ERROR: ${template_dir} exists, but ${output_dir} is not writable"
     return 0
   fi
-  find "$template_dir" -follow -type f -name "*$suffix" -print | while read -r template; do
-    relative_path="${template#"$template_dir/"}"
-    output_path="$output_dir/${relative_path%"$suffix"}"
-    subdir=$(dirname "$relative_path")
+  find "${template_dir}" -follow -type f -name "*${suffix}" -print | while read -r template; do
+    relative_path="${template#"${template_dir}/"}"
+    output_path="${output_dir}/${relative_path%"${suffix}"}"
+    subdir=$(dirname "${relative_path}")
     # create a subdirectory where the template file exists
-    mkdir -p "$output_dir/$subdir"
-    entrypoint_log "$ME: Running envsubst on $template to $output_path"
-    envsubst "$defined_envs" < "$template" > "$output_path"
+    mkdir -p "${output_dir}/${subdir}"
+    entrypoint_log "${ME}: Running envsubst on ${template} to ${output_path}"
+    envsubst "${defined_envs}" < "${template}" > "${output_path}"
   done
 
   # Print the first file with the stream suffix, this will be false if there are none
-  if test -n "$(find "$template_dir" -name "*$stream_suffix" -print -quit)"; then
-    mkdir -p "$stream_output_dir"
-    if [ ! -w "$stream_output_dir" ]; then
-      entrypoint_log "$ME: ERROR: $template_dir exists, but $stream_output_dir is not writable"
+  if test -n "$(find "${template_dir}" -name "*${stream_suffix}" -print -quit)"; then
+    mkdir -p "${stream_output_dir}"
+    if [ ! -w "${stream_output_dir}" ]; then
+      entrypoint_log "${ME}: ERROR: ${template_dir} exists, but ${stream_output_dir} is not writable"
       return 0
     fi
     add_stream_block
-    find "$template_dir" -follow -type f -name "*$stream_suffix" -print | while read -r template; do
-      relative_path="${template#"$template_dir/"}"
-      output_path="$stream_output_dir/${relative_path%"$stream_suffix"}"
-      subdir=$(dirname "$relative_path")
+    find "${template_dir}" -follow -type f -name "*${stream_suffix}" -print | while read -r template; do
+      relative_path="${template#"${template_dir}/"}"
+      output_path="${stream_output_dir}/${relative_path%"${stream_suffix}"}"
+      subdir=$(dirname "${relative_path}")
       # create a subdirectory where the template file exists
-      mkdir -p "$stream_output_dir/$subdir"
-      entrypoint_log "$ME: Running envsubst on $template to $output_path"
-      envsubst "$defined_envs" < "$template" > "$output_path"
+      mkdir -p "${stream_output_dir}/${subdir}"
+      entrypoint_log "${ME}: Running envsubst on ${template} to ${output_path}"
+      envsubst "${defined_envs}" < "${template}" > "${output_path}"
     done
   fi
 }
