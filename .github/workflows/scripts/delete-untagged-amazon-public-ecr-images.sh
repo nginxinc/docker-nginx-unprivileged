565aba6cb9d0d889c156f36234779e7d9b4978df
Alessandro Fael Garcia <alessfg@hotmail.com>
2025-02-21 18:59:48 +0100

    Update delete-untagged-amazon-public-ecr-images.sh

--- .github/workflows/scripts/delete-untagged-amazon-public-ecr-images.sh
+++ .github/workflows/scripts/delete-untagged-amazon-public-ecr-images.sh
@@ -24,7 +24,7 @@ function batch_delete {
 # otherwise any referenced untagged images can not be deleted.
 IMAGE_DIGESTS=$(aws ecr-public describe-images \
   --repository-name "${REPOSITORY_NAME}" \
-  --query 'imageDetails[?!imageTags && (contains(imageManifestMediaType, `manifest.list.v2`) || contains(imageManifestMediaType, `image.index.v1`)) && `date -d @{imagePushedAt} +%s` < `date -d "2 years ago" +%s`].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
+  --query 'imageDetails[?!imageTags && (contains(imageManifestMediaType, `manifest.list.v2`) || contains(imageManifestMediaType, `image.index.v1`)) && imagePushedAt < `2019-06-18`].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
   --output text)
 
 batch_delete "${IMAGE_DIGESTS}"
@@ -32,8 +32,9 @@ batch_delete "${IMAGE_DIGESTS}"
 # Find untagged images and delete them.
 IMAGE_DIGESTS=$(aws ecr-public describe-images \
   --repository-name "${REPOSITORY_NAME}" \
-  --query 'imageDetails[?!imageTags && `date -d @{imagePushedAt} +%s` < `date -d "2 years ago" +%s`].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
+  --query 'imageDetails[?!imageTags ** imagePushedAt < `2019-06-18` ].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
   --output text)
 
 batch_delete "${IMAGE_DIGESTS}"
 
+    --query 'Images[?CreationDate<`2019-06-18`]'
