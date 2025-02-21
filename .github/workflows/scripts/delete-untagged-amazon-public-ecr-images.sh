66804a4f248543d960b038a86589571f98059acb
Alessandro Fael Garcia <alessfg@hotmail.com>
2025-02-21 19:03:25 +0100

    Update delete-untagged-amazon-public-ecr-images.sh

--- .github/workflows/scripts/delete-untagged-amazon-public-ecr-images.sh
+++ .github/workflows/scripts/delete-untagged-amazon-public-ecr-images.sh
@@ -6,7 +6,8 @@ set -eu
 
 REPOSITORY_NAME=nginx-unprivileged
 BATCH_DELETE_SIZE=100 # The max delete size allowed by the 'batch-delete-image' AWS CLI command is 100
-CUTOFF_DATE=$(date -d '2 years ago' +%s)
+CUTOFF_DATE="2019-06-18"
+# CUTOFF_DATE=$(date -d '2 years ago' +%s)
 
 function batch_delete {
   while read -r batch; do
@@ -24,7 +25,7 @@ function batch_delete {
 # otherwise any referenced untagged images can not be deleted.
 IMAGE_DIGESTS=$(aws ecr-public describe-images \
   --repository-name "${REPOSITORY_NAME}" \
-  --query 'imageDetails[?!imageTags && (contains(imageManifestMediaType, `manifest.list.v2`) || contains(imageManifestMediaType, `image.index.v1`)) && imagePushedAt < `2019-06-18`].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
+  --query 'imageDetails[?!imageTags && (contains(imageManifestMediaType, `manifest.list.v2`) || contains(imageManifestMediaType, `image.index.v1`)) && imagePushedAt < `'$CUTOFF_DATE'`].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
   --output text)
 
 batch_delete "${IMAGE_DIGESTS}"
@@ -32,7 +33,7 @@ batch_delete "${IMAGE_DIGESTS}"
 # Find untagged images and delete them.
 IMAGE_DIGESTS=$(aws ecr-public describe-images \
   --repository-name "${REPOSITORY_NAME}" \
-  --query 'imageDetails[?!imageTags && imagePushedAt < `2019-06-18` ].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
+  --query 'imageDetails[?!imageTags && imagePushedAt < `'$CUTOFF_DATE'` ].{imageDigest: join(`=`, [`imageDigest`, imageDigest])}' \
   --output text)
 
 batch_delete "${IMAGE_DIGESTS}"
